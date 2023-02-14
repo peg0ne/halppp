217b8ed4fbc8b0f78156dd194e375f200453d67d
peg0ne <oliverjonasson@gmail.com>
2023-02-14 20:54:48 +0100

    $update rebuild step

--- compiler/rebuild.sh
+++ compiler/rebuild.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 cargo run main.ha && ./main main.ha -r && ./main main.ha -r
 mkdir -p $HOME/.local/lib/halppp
-cp ../lib/*.ha $HOME/.local/lib/halppp
-
+cp -r ../lib/* $HOME/.local/lib/halppp
